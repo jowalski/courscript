ea6ffc40b07a80a1da6aabe24ab88d783c6e06aa
Jowalski <jowalski@gmail.com>
2015-10-24 11:12:43 -0400

    Re-implemented functionality in classes

---
+++ courscript/courscript.py
@@ -0,0 +1,22 @@
+from folder import CourseFilelist
+from header import CourseHeaderList
+from sub import CourseSublist
+from para import CourseParalist
+import sys
+
+
+class Courscript:
+
+    def __init__(self, title_dir, path="*/*.srt", base=1):
+        self.title_dir = title_dir
+        self.srt_path = path
+        self.filelist = CourseFilelist(self.title_dir, self.srt_path)
+        self.headerlist = CourseHeaderList(self.filelist, base)
+
+    def print(self, file=sys.stdout, base=1):
+        """Print a md file of subtitle transcripts.
+        """
+        for headers, srt_file in zip(self.headerlist, self.filelist):
+            srtlst = CourseSublist(srt_file.path)
+            paras = CourseParalist(srtlst, headers)
+            paras.print(file)
