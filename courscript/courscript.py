167580d9823d2974f3c4baa9a173904e2e5a1b2f
Jowalski <jowalski@gmail.com>
2015-11-06 18:55:31 -0500

    Add slide functionality to main class

--- courscript/courscript.py
+++ courscript/courscript.py
@@ -1,22 +1,40 @@
 from courscript.folder import CourseFilelist
-from courscript.header import CourseHeaderList
-from courscript.sub import CourseSublist
-from courscript.para import CourseParalist
+from courscript.header import HeaderList
+from courscript.sub import Sublist
+from courscript.para import Paralist
+from courscript.slide import CoursePdflist
+import os.path
 import sys
 
 
 class Courscript:
 
-    def __init__(self, title_dir, path="*/*.srt", base=1):
+    def __init__(self, title_dir, path="*/*.srt", base=1,
+                 split=u'_', sub=u'-', pdfpath=False):
         self.title_dir = title_dir
         self.srt_path = path
-        self.filelist = CourseFilelist(self.title_dir, self.srt_path)
-        self.headerlist = CourseHeaderList(self.filelist, base)
+        self.pdfpath = pdfpath
+        self.filelist = CourseFilelist(title_dir, path, split, sub)
+        self.headerlist = HeaderList(self.filelist, base)
+        self.ispdf = False if not pdfpath else True
+        self.pdflist = None if not pdfpath else \
+            CoursePdflist(title_dir, pdfpath)
 
     def print(self, file=sys.stdout, base=1):
         """Print a md file of subtitle transcripts.
         """
+        if self.ispdf:
+            self._print_wslides(file, base)
         for headers, srt_file in zip(self.headerlist, self.filelist):
-            srtlst = CourseSublist(srt_file.path)
-            paras = CourseParalist(srtlst, headers)
+            srtlst = Sublist(srt_file.path)
+            paras = Paralist(srtlst, headers)
+            paras.print(file)
+
+    def _print_wslides(self, file=sys.stdout, base=1):
+        print('Adding slides...')
+        for headers, srt_file, pdf in \
+                zip(self.headerlist, self.filelist, self.pdflist):
+            srtlst = Sublist(srt_file.path)
+            slidelist = pdf.slist
+            paras = Paralist(srtlst, headers, slidelist)
             paras.print(file)
